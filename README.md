3ec4aeebefd1d43389bf52bf989fa29ada847f69
HenriquePNZ <h254260@dac.unicamp.br>
2023-08-22 20:08:59 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Calculadora em JavaScript
+
+### Primeiro código utilizando a linguagem JS proposto pelo BootCamp da DIO
+
+### O código se baseia em uma calculadora composta por alertas na tela do usuário. Utilizando funções, estruturas condicionais e impedindo entradas erradas por parte do usuário.# Calculadora-em-JavaScript
---
+++ tiposDeFuncao.js
@@ -0,0 +1,104 @@
+
+function calculadora () {
+
+    const operacao = Number(prompt ('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão real (/)\n 5 - Divisão inteira (%)\n 6 - Potenciação (**)'));
+
+    if (!operacao || operacao >= 7){
+        alert ('Erro - parâmetros inválida');
+        calculadora ();
+    }
+
+    else {
+
+         let valor1 = Number (prompt('Insira o primeiro valor:'));
+            
+         if (!valor1){
+            alert ('Erro - parâmetros inválidos')
+            calculadora ();
+         }
+        
+        let valor2 = Number (prompt('Insira o segundo valor:'));
+        
+        if (!valor2){
+            alert ('Erro - parâmetros inválidos')
+            calculadora ();
+        }
+
+        let resultado; 
+
+
+        function Soma (){
+            resultado = valor1 + valor2;
+            alert (`${valor1} + ${valor2} = ${resultado}`)
+            novaOperacao ();
+        }
+        function Subtração (){
+            resultado = valor1 - valor2;
+            alert (`${valor1} - ${valor2} = ${resultado}`)
+            novaOperacao();
+        }
+        function Multiplicação (){
+            resultado = valor1 * valor2;
+            alert (`${valor1} * ${valor2} = ${resultado}`)
+            novaOperacao();
+        }
+        function divisãoReal (){
+            resultado = valor1 / valor2;
+            alert (`${valor1} / ${valor2} = ${resultado}`)
+            novaOperacao ();
+        }
+        function divisãoInteira (){
+            resultado = valor1 % valor2;
+            alert (`${valor1} % ${valor2} = ${resultado}`)
+            novaOperacao();
+        }
+        function potenciação (){
+            resultado = valor1 ** valor2;
+            alert (`${valor1} ** ${valor2} = ${resultado}`)
+            novaOperacao ();
+        }
+    
+        function novaOperacao (){
+            let opcao = prompt ('Deseja realizar outra operação?\n 1 - Sim\n 2 - Não');
+    
+            if (opcao == 1){
+                calculadora ();
+            }
+    
+            else if (opcao == 2){
+                alert ('Até mais!');
+            }
+    
+            else {
+                alert ('Digite uma opção válida!');
+                calculadora ();
+            }
+        }
+    
+        if (operacao == 1){
+            Soma ();
+        }
+    
+        else if (operacao == 2){
+            Subtração ();
+        }
+    
+        else if (operacao == 3){
+            Multiplição ();
+        }
+    
+        else if (operacao == 4){
+            divisãoReal ();
+        }
+    
+        else if (operacao == 5){
+            divisãoInteira ();
+        }
+    
+        else if (operacao ==6){
+            potenciação ();
+        }
+    }
+}
+
+calculadora ();
